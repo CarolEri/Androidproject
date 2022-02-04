1813431a82d34bd5249bb3c42835ce10361ee475
˜Carol˜ <eriushirobira@hotmail.com>
2022-02-04 17:40:31 -0300

    Adding Alert Button

--- app/src/main/java/com/example/projetobuser/ui/SearchScreen.java
+++ app/src/main/java/com/example/projetobuser/ui/SearchScreen.java
@@ -1,8 +1,12 @@
 package com.example.projetobuser.ui;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.projetobuser.R;
 
@@ -15,5 +19,27 @@ public class SearchScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_screen2);
         Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+
+    public void openDialog(View view){
+
+        //INSTANCIAR ALERT DIALOG
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
+        //CONFIGURAR TÍTULO MENSAGEM
+        dialog.setTitle("Notificações");
+        dialog.setMessage("Você não possui notificações no momento.");
+
+        //CONFIGURAR SIM E NAO
+        dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        //CRIAR E EXIBIR A DIALOG
+        dialog.create();
+        dialog.show();
     }
 }
