d29010c9f30e119bf922e29bcafc60fdd5271f79
˜Carol˜ <eriushirobira@hotmail.com>
2022-02-04 15:31:19 -0300

    Adding navigation from login to main screen

--- app/src/main/java/com/example/projetobuser/LoginForm.java
+++ app/src/main/java/com/example/projetobuser/LoginForm.java
@@ -6,11 +6,14 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.projetobuser.ui.SearchScreen;
+
 import java.util.Objects;
 
 public class LoginForm extends AppCompatActivity {
 
     private TextView text_RegisterForm;
+    private androidx.appcompat.widget.AppCompatButton login_botao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +27,16 @@ public class LoginForm extends AppCompatActivity {
             Intent intent = new Intent(LoginForm.this, RegisterForm.class);
             startActivity(intent);
         });
+
+        login_botao.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginForm.this, SearchScreen.class);
+            startActivity(intent);
+        });
     }
 
     private void StartComponents(){
         text_RegisterForm = findViewById(R.id.cadastre_se);
+        login_botao = findViewById(R.id.botao_entrar);
     }
+
 }
--- app/src/main/java/com/example/projetobuser/ui/SearchScreen.java
+++ app/src/main/java/com/example/projetobuser/ui/SearchScreen.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 
 import com.example.projetobuser.R;
 
+import java.util.Objects;
+
 public class SearchScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_screen2);
+        Objects.requireNonNull(getSupportActionBar()).hide();
     }
 }
