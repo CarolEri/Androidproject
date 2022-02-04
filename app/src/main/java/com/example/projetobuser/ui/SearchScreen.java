3c9348bd2278a63cd792367ed025abed202df771
˜Carol˜ <eriushirobira@hotmail.com>
2022-02-04 15:14:31 -0300

    Creating the third screen and testing some stuff

---
+++ app/src/main/java/com/example/projetobuser/ui/SearchScreen.java
@@ -0,0 +1,16 @@
+package com.example.projetobuser.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projetobuser.R;
+
+public class SearchScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_screen2);
+    }
+}
