4a975802e4676ff963e6392fdf130ff7a24d9081
˜Carol˜ <eriushirobira@hotmail.com>
2022-02-07 10:24:54 -0300

    Adding "back button" to Register screen

--- app/src/main/java/com/example/projetobuser/RegisterForm.java
+++ app/src/main/java/com/example/projetobuser/RegisterForm.java
@@ -2,11 +2,14 @@ package com.example.projetobuser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.Objects;
 
 public class RegisterForm extends AppCompatActivity {
+
+    private androidx.appcompat.widget.AppCompatButton voltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,15 @@ public class RegisterForm extends AppCompatActivity {
         setContentView(R.layout.activity_register_form);
 
         Objects.requireNonNull(getSupportActionBar()).hide();
+        StartComponents();
+
+        voltar.setOnClickListener(v -> {
+            Intent intent = new Intent(RegisterForm.this, LoginForm.class);
+            startActivity(intent);
+        });
+    }
+
+    private void StartComponents(){
+        voltar = findViewById(R.id.botao_voltar);
     }
 }
